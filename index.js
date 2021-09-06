0709e20f8cb4c32a0e86906d8c8d4449a63a952b
Martin <martintonts@gmail.com>
2021-09-06 17:33:22 +0300

    init

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const express = require('express');
+const got = require('got');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+
+const app = express();
+
+async function fetch_data() {
+  return (await got('https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%0A(%0A%20%20node%5B!shop%5D%5Bamenity!%3Dpharmacy%5D%5Bamenity%5D(%7B%7Bbbox%7D%7D)%3B%0A%20%20node%5Btourism%5D(%7B%7Bbbox%7D%7D)%3B%0A%20%20node%5Bhistoric%5D(%7B%7Bbbox%7D%7D)%3B%0A)%3B%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B', {responseType: 'json', resolveBodyOnly: true}));
+}
+
+function getNow() {
+  return new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+}
+
+let data;
+
+app.get('/', async (req, res) => {
+  console.log(getNow() + ' - Client connected');
+  res.send(data);
+});
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+  while (!data || !data.elements || !data.elements.length) {
+    try {
+      console.log("Overpass API status:");
+      console.log((await got('https://overpass-api.de/api/status', {resolveBodyOnly: true})));
+      console.log("Trying to fetch data...");
+      data = await fetch_data();
+      if (!data.elements.length) console.log(data.remark);
+    } catch(error) {
+      console.error(error.response.body);
+    }
+    await sleep(10000);
+  }
+
+  app.listen(PORT, HOST, () => console.log(`Running on http://${HOST}:${PORT}`));
+}
+
+main();
+
